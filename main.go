8777213050324c292fb54b0e51665fb2204d39f8
cosin_lu <cosin_lu@163.com>
2018-06-03 21:17:33 +0800

    cosin

--- main.go
+++ main.go
@@ -2,13 +2,36 @@ package main
 
 import (
 	"fmt"
-    "github.com/garyburd/redigo/redis"//redis
     _"github.com/go-sql-driver/mysql"
     "github.com/gorilla/websocket"
     "time"
     "net/http"
     "log"
+    "reflect"
 )
+
+const (
+    // Time allowed to write a message to the peer.
+    writeWait = 10 * time.Second
+
+    // Time allowed to read the next pong message from the peer.
+    pongWait = 60 * time.Second
+
+    // Send pings to peer with this period. Must be less than pongWait.
+    pingPeriod = (pongWait * 9) / 10
+
+    // Maximum message size allowed from peer.
+    maxMessageSize = 512
+)
+
+//设定一个解析数据请求的格式
+type requestData struct {
+    Event string `json:"event"`
+    RoomId int `json:"roomid"`
+    Uid int `json:"uid"`
+    Lon string `json:"lon"`
+    Lat string `json:"lat"`
+}
 
 var upgrader = websocket.Upgrader{
     ReadBufferSize:   1024,
@@ -21,22 +44,24 @@ var upgrader = websocket.Upgrader{
 
 type Hub struct {
     clients map[*Client]bool
-    broadcast chan []byte
+    broadcast chan requestData
     register chan *Client
     unregister chan *Client
+    rooms map[int][]int
 }
 
 type Client struct {
     hub *Hub
     conn *websocket.Conn
-    send chan []byte
+    send chan requestData
 }
 
 var manager = Hub{
-    broadcast:  make(chan []byte),
-    register:   make(chan *Client),
+    broadcast: make(chan requestData),
+    register: make(chan *Client),
     unregister: make(chan *Client),
-    clients:    make(map[*Client]bool),
+    clients: make(map[*Client]bool),
+    rooms: make(map[int][]int),
 }
 
 func (h *Hub) run() {
@@ -46,15 +71,19 @@ func (h *Hub) run() {
             fmt.Println("有新链接加入",client)
 			h.clients[client] = true
 		case client := <-h.unregister://有链接断开
+		    fmt.Println("发完就断开了？")
 			if _, ok := h.clients[client]; ok {
 				delete(h.clients, client)
 				close(client.send)
 			}
 		case message := <-h.broadcast://有新消息
+		fmt.Println("有数据的",message,reflect.TypeOf(message))
 			for client := range h.clients {
 				select {
 				case client.send <- message:
+				    fmt.Println("检查发送的消息是什么",message)
 				default:
+				    fmt.Println("会每次都关闭吗")
 					close(client.send)
 					delete(h.clients, client)
 				}
@@ -69,21 +98,11 @@ func res(hub *Hub,w http.ResponseWriter,r *http.Request) {
         fmt.Println("错误",err)
         log.Fatal(err)
     }
-    defer ws.Close()
-    client := &Client{hub:hub,conn:ws,send:make(chan []byte,256)}
+    client := &Client{hub:hub,conn:ws,send:make(chan requestData)}
+    fmt.Println("应该是从这边先进入的链接")
     client.hub.register <- client
-	for {
-		_, message, err := client.conn.ReadMessage()
-		if err != nil {
-			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
-				log.Printf("error: %v", err)
-			}
-			break
-		}
-		client.hub.broadcast <- message
-        fmt.Println("数据",string(message),err)
-        fmt.Println("链接",hub.clients)
-	}
+    go client.readPump()
+    //go client.writePump()
 }
 
 func main(){
@@ -95,33 +114,76 @@ func main(){
     hub := &manager
     go hub.run()
     mux := http.NewServeMux()
-	mux.HandleFunc("/res", func(w http.ResponseWriter, r *http.Request) {
+    mux.HandleFunc("/res", func(w http.ResponseWriter, r *http.Request) {
         fmt.Println("???")
-		res(hub, w, r)
-	})
+        res(hub, w, r)
+    })
     log.Println("Listening...")
     http.ListenAndServe(":8800",mux)
 }
 
-func redis_example(){
-    c, err := redis.Dial("tcp", "127.0.0.1:6379")
-    checkErr(err)
-    defer c.Close()
-    c.Do("auth","cosin")
-    /* _, err = c.Do("SET", "mykey", "superWang")
-    if err != nil {
-        fmt.Println("redis set failed:", err)
-    } */
-    username, err := redis.String(c.Do("GET", "author"))
-    if err != nil {
-        fmt.Println("redis get failed:", err)
-    } else {
-        fmt.Printf("Get mykey: %v \n", username)
+func (c *Client) readPump() {
+    defer func() {
+        c.hub.unregister <- c
+        c.conn.Close()
+    }()
+    c.conn.SetReadLimit(maxMessageSize)
+    c.conn.SetReadDeadline(time.Now().Add(pongWait))
+    c.conn.SetPongHandler(func(string) error { c.conn.SetReadDeadline(time.Now().Add(pongWait)); return nil })
+    for {
+        var request requestData
+        err := c.conn.ReadJSON(&request)
+        if err != nil {
+            fmt.Println("难道是读取时候出错了吗",err)
+            if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+                log.Printf("error: %v", err)
+            }
+            break
+        }
+        if err != nil {
+            fmt.Println("读取json数据失败",err)
+        }
+        fmt.Println("循环读取数据",request,request.Event,err,reflect.TypeOf(request))
+        c.hub.broadcast <- request
     }
 }
 
-func checkErr(err error){
-    if err != nil {
-        panic(err)
+/*func (c *Client) writePump() {
+    ticker := time.NewTicker(pingPeriod)
+    defer func() {
+        ticker.Stop()
+        c.conn.Close()
+    }()
+    for {
+        select {
+        case message, ok := <-c.send:
+            c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+            if !ok {
+                // The hub closed the channel.
+                c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+                return
+            }
+
+            w, err := c.conn.NextWriter(websocket.TextMessage)
+            if err != nil {
+                return
+            }
+            w.Write(message)
+
+            // Add queued chat messages to the current websocket message.
+            n := len(c.send)
+            for i := 0; i < n; i++ {
+                w.Write(<-c.send)
+            }
+
+            if err := w.Close(); err != nil {
+                return
+            }
+        case <-ticker.C:
+            c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+            if err := c.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+                return
+            }
+        }
     }
-}
+}*/
